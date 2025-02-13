02c4317870b4a0c4c62501c1a7108abc1b0c55f6
EgorKancir <kancyregor@gmail.com>
2025-02-13 18:30:35 +0100

    svg

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -288,7 +288,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54063" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62025" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
